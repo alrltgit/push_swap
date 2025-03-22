69070736f4d57c8ff92bdf49436c2e22258f5d80
Alina <ralcevichalina70@gmail.com>
2025-03-22 19:15:25 +0100

    Added an algorithm to sort 6-99 arguments

--- sort_cases/case_4.c
+++ sort_cases/case_4.c
@@ -6,72 +6,11 @@
 /*   By: apple <apple@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 17:35:05 by apple             #+#    #+#             */
-/*   Updated: 2025/03/21 19:37:15 by apple            ###   ########.fr       */
+/*   Updated: 2025/03/22 19:11:57 by apple            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
-
-// t_stack *next_num(t_stack **stack)
-// {
-//     t_stack *temp;
-
-//     temp = *stack;
-//     while (temp->next)
-//         temp = temp->next;
-//     return (temp);
-// }
-
-// void find_location_in_b(t_size *s, t_stack **stack_a, t_stack **stack_b)
-// {
-//     t_stack *current_a;
-//     t_stack *current_b;
-
-//     if (s->b_size == 0)
-//     {
-//         push_b(s, stack_a, stack_b);
-//         return ;
-//     }
-//     current_a = *stack_a;
-//     current_b = *stack_b;
-//     if (current_a->data > current_b->data)
-//     {
-//         push_b(s, stack_a, stack_b);
-//         return ;
-//     }
-//     else if (current_a->data < next_num(stack_b)->data)
-//     {
-//         push_b(s, stack_a, stack_b);
-//         rotate_b(stack_b);
-//     }
-//     else if (current_a->data < current_b->data)
-//     {
-//         current_b = *stack_b;
-//         while (current_b && !(current_a->data < current_b->data && current_a->data > current_b->next->data))
-//         {
-//             rotate_b(stack_b);
-//             current_b = *stack_b;
-//         }
-//         push_b(s, stack_a, stack_b);
-//         swap_b(stack_b);
-//         if (stack_b_is_sorted(stack_b))
-//             return ;
-//         else
-//         {
-//             current_b = *stack_b;
-//             while (current_b->next)
-//                 current_b = current_b->next;
-//             while (current_b && current_b->data < current_b->prev->data)
-//             {
-//                 reverse_rotate_b(stack_b);
-//                 current_b = next_num(stack_b);
-//             }
-//             if (current_b->data > current_b->prev->data)
-//                 reverse_rotate_b(stack_b);
-//             return ;
-//         }
-//     }
-// }
 
 int find_lowest_first(t_stack **stack_a)
 {
@@ -89,39 +28,6 @@ int find_lowest_first(t_stack **stack_a)
         temp = temp->next;
     }
     return (hold_first);
-}
-
-int find_lowest_second(t_stack **stack_a)
-{
-    t_stack *temp;
-    int hold_second;
-    int len;
-    int i;
-
-    temp = *stack_a;
-    len = 0;
-    while (temp)
-    {
-        len++;
-        temp = temp->next;
-    }
-    temp = *stack_a;
-    i = 0;
-    while (i < len - 1)
-    {
-        temp = temp->next;
-        i++;
-    }
-    hold_second = temp->data;
-    while (temp)
-    {
-        if (temp->data < hold_second)
-        {
-            hold_second = temp->data;
-        }
-        temp = temp->prev;
-    }
-    return(hold_second);
 }
 
 int find_lowest_to_zero(int hold_first_idx)
@@ -153,10 +59,9 @@ void case_4(t_size *s, t_stack **stack_a, t_stack **stack_b)
 {
     int chunk;
     int num_chunks;
-    int chunk_size;
+    long chunk_size;
     t_stack *temp;
     int lowest_first_idx = 0;
-    int lowest_second_idx = 0;
     int lowest_to_max;
     int lowest_to_zero;
     
@@ -168,46 +73,55 @@ void case_4(t_size *s, t_stack **stack_a, t_stack **stack_b)
     else if (s->a_size <= 99)
         num_chunks = 5;
 
-    (void)stack_b;
     chunk_size = s->a_size / num_chunks;
     chunk = 0;
-    while (chunk < chunk_size)
+    while (chunk < num_chunks)
     {
-        int j = 0;
-        while (j < s->a_size)
+        int min = chunk * chunk_size;
+        int max = chunk_size + min - 1;
+        int j = min;
+        while (j <= max)
         {
-             // int min = chunk * chunk_size;
-            // int max = chunk_size + min - 1;
             int lowest_first = find_lowest_first(stack_a);
-            int lowest_second = find_lowest_second(stack_a);
-            if (!lowest_first || !lowest_second)
+            if (!lowest_first)
             {
                 ft_printf("Lowest number is not found.\n");
                 return ;
             }
-            ft_printf("lowest_first: %d\n lowest_second: %d\n", lowest_first, lowest_second);
             temp = *stack_a;
             int i = 0;
             while (temp)
             {
                 if (temp->data == lowest_first)
                     lowest_first_idx = i;
-                if (temp->data == lowest_second)
-                    lowest_second_idx = i;
                 temp = temp->next;
                 i++;
             }
-            ft_printf("lowest_first_idx: %d\n lowest_second_idx: %d\n", lowest_first_idx, lowest_second_idx);
             lowest_to_zero = find_lowest_to_zero(lowest_first_idx);
-            lowest_to_max = find_lowest_to_max(s, lowest_second_idx);
-            ft_printf("lowest_to_zero: %d\n lowest_to_max: %d\n", lowest_to_zero, lowest_to_max);
+            lowest_to_max = find_lowest_to_max(s, lowest_first_idx);
             if (lowest_to_zero > lowest_to_max)
-                reverse_rotate_idx(s, stack_a, lowest_second_idx);
+                reverse_rotate_idx(s, stack_a, lowest_first_idx);
             else
                 rotate_idx(stack_a, lowest_first_idx);
+            push_b(s, stack_a, stack_b);
             j++;
         }
-        push_b(s, stack_a, stack_b);
         chunk++;
+    }
+    while (s->a_size > 0)
+    {
+        int lowest = find_lowest_first(stack_a);
+        int i = 0;
+        int idx = 0;
+        temp = *stack_a;
+        while (temp)
+        {
+            if (temp->data == lowest)
+                idx = i;
+            temp = temp->next;
+            i++;
+        }
+        rotate_idx(stack_a, idx);
+        push_b(s, stack_a, stack_b);
     }
 }
